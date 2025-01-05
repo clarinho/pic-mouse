c83c263aebf14e2711b814af19528ae1f38a1cc7
Your Name <you@example.com>
2025-01-05 11:00:00 +0000

    Set up basic project folder structure

--- main.c
+++ main.c
@@ -6,3 +6,10 @@
                 case 0x40BF08F7: // left
 
 }
+#include <xc.h>
+// External interrupt for IR
+                setup_timer_1(T1_DISABLED);
+                if (t > 1000)
+void t3_isr(void) {
+                    mousePacket[1] = -1;
+    TRISB0 = 1; // IR receiver (INT0)
